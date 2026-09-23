640456e37bcb7efafd868006c7f276a166850fab
Banane_Rotative <banane.orbitale@gmail.com>
2026-09-23 11:45:40 +0200

    Add: minimal python integration as example

---
+++ PyCeleste/main.py
@@ -0,0 +1,17 @@
+from celeste_connector import CelesteConnector
+from CelestePythonInterface import SessionData
+from controller import Controller
+
+celeste_conn = CelesteConnector()
+celeste_conn.connect()
+
+controller = Controller()
+celeste_conn.set_agent(controller.update)
+
+while True:     # Control goes back here after each player death
+    final_state = celeste_conn.run()
+    
+    print("\n=== Session terminée ===")
+    print(f"Frames écoulées   : {final_state[SessionData.NUMBER_OF_LEVELS_FINISHED.value]}")
+    print(f"Temps (s)         : {final_state[SessionData.SECONDS_ELAPSED.value]:.2f}")
+    print(f"Distance objectif : {final_state[SessionData.DISTANCE_TO_OBJECTIVE.value]:.1f}")
